3408e7d1dc737c9f731896c1d83c3f981830f05e
Ed <pushpoped@gmail.com>
2021-10-24 19:36:59 -0400

    adding first view

--- PokeMvx.Droid/Views/MainView.cs
+++ PokeMvx.Droid/Views/MainView.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using Android.OS;
+using MvvmCross.Platforms.Android.Views;
+using PokeMvx.Core.ViewModels;
+using MvvmCross.Binding.BindingContext;
+using Android.App;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+
 namespace PokeMvx.Droid.Views
 {
-    public class MainView
+    [MvxActivityPresentation]
+    [Activity]
+    public class MainView : MvxActivity<MainViewViewModel>
     {
-        public MainView()
+        protected override void OnCreate(Bundle savedInstanceState)
         {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.main_view);
+
+            //create date picker and bindings
+            var set = this.CreateBindingSet<Views.MainView, MainViewViewModel>();
+            set.Bind(SupportActionBar).For(v => v.Title).To(vm => vm.Title);           
+            set.Apply();
+
         }
     }
 }
